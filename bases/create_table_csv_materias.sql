2b54c5cc134ab5404c2766f5c487896c090af0ee
alexsh_home <alexsh882@gmail.com>
2023-11-20 20:31:39 -0300

    Crear tabla materias para importar desde s desde csv

---
+++ bases/create_table_csv_materias.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS public.dea_local_materias;
+
+CREATE TABLE
+    public.dea_local_materias (
+        _id character varying(20),
+        plan_id character varying(10),
+        espacio character varying(255)
+    );
