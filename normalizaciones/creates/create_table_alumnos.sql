3bd0e1b2b64d07a0b3c3dd40256f1117c0b328a5
alexsh_home <alexsh882@gmail.com>
2023-11-20 21:33:46 -0300

    Creación de tabla e inserción de datos a tabla alumnos.

---
+++ normalizaciones/creates/create_table_alumnos.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS public.alumnos;
+
+CREATE TABLE
+    public.alumnos (
+        _id integer,
+        apellido character varying(50),
+        nombre character varying(50),
+        sexo character varying(2),
+        nacionalidad character varying(20),
+        fecha_nacim character varying(25)
+    )
---
+++ normalizaciones/inserts_and_updates/insert_to_alumnos.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+    alumnos
+SELECT
+    _id,
+    apellido,
+    nombre,
+    sexo,
+    nacionalidad,
+    fecha_nacim
+FROM
+    dea_local_alumnos
