2a780b7129a2e52a2b6f1cb80a1c8d1f8eb239d2
alexsh882 <alexsh882@gmail.com>
2023-11-21 12:04:10 -0300

    Creación de tabla e inserción de datos a tabla planes.

---
+++ normalizaciones/creates/create_table_planes.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS public.planes;
+
+CREATE TABLE
+    public.planes (
+        _id character varying(10),
+        carrera character varying(150),
+        fplan character varying(25),
+        anios smallint
+    )
---
+++ normalizaciones/inserts_and_updates/insert_to_planes.sql
@@ -0,0 +1,9 @@
+insert into
+    public.planes (_id, carrera, fplan, anios)
+SELECT
+    _id,
+    carrera,
+    fplan,
+    anios
+from
+    dea_local_planes
